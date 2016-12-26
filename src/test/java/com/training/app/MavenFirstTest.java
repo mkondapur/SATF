9c059b04d7653320fd1205364aef26a677afa6b4
mkondapur <mkondapur@gmail.com>
2016-12-26 18:25:35 +0530

    Added new Maven automation test framework

---
+++ src/test/java/com/training/app/MavenFirstTest.java
@@ -0,0 +1,52 @@
+package com.training.app;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class MavenFirstTest {
+
+	WebDriver driver;
+	@BeforeClass
+	public void setup() throws Exception{
+		
+		try {
+			System.setProperty("webdriver.firefox.bin", "C:\\Program Files\\Mozilla Firefox\\firefox.exe");
+			driver = new FirefoxDriver();
+			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+			
+			
+		} catch (Exception e) {
+			
+			throw new Exception("FAILED WHILE INTIALIZING BROWSER"+e.getLocalizedMessage());
+		}
+		
+	}
+@Test
+	public void verify_Test() throws Exception{
+		
+		try {
+			driver.get("https://www.google.co.in/");
+			String text = driver.getTitle();
+			System.out.println("HomePage title:"+text);
+		} catch (Exception e) {
+			throw new Exception("Failed while verifying:verify_Test"+e.getLocalizedMessage());
+		}
+	}
+
+	@AfterClass
+	public void tearDown() throws Exception{
+		
+		try {
+			driver.quit();
+		} catch (Exception e) {
+			throw new Exception("FAILED WHILE TERMINATIONG BROWSER"+e.getLocalizedMessage());
+		}
+	}
+}
+
